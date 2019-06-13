cd15343c9e950b4246653e37228e2dad5571b50e
Pablo <pablo10544@hotmail.com>
2019-06-13 14:37:42 -0300

    Add files via upload

---
+++ DESAFIO COMPLEXO LAURO/DESAFIO COMPLEXO LAURO/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace DESAFIO_COMPLEXO_LAURO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNome = new System.Windows.Forms.Label();
+            this.tbxNome = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(51, 108);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "Nome";
+            // 
+            // tbxNome
+            // 
+            this.tbxNome.Location = new System.Drawing.Point(138, 108);
+            this.tbxNome.Name = "tbxNome";
+            this.tbxNome.Size = new System.Drawing.Size(100, 20);
+            this.tbxNome.TabIndex = 1;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(96, 171);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(110, 44);
+            this.btnSalvar.TabIndex = 2;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(135, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Salvar Nome";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 252);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.tbxNome);
+            this.Controls.Add(this.lblNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox tbxNome;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DESAFIO COMPLEXO LAURO/DESAFIO COMPLEXO LAURO/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DESAFIO_COMPLEXO_LAURO
+{
+    public partial class Form1 : Form
+    {
+        string nome = "";
+        public Form1()
+        {
+            InitializeComponent();
+            carregarTxt();
+        }
+
+        private void carregarTxt()
+        {
+            if (File.Exists("nome.txt"))
+            {
+                Stream stream = File.Open("nome.txt", FileMode.Open);
+                StreamReader leitor = new StreamReader(stream);
+                string linha = leitor.ReadLine();
+                while (linha!=null)
+                {
+                    tbxNome.Text = linha;
+                    linha = leitor.ReadLine();
+                } 
+                leitor.Close();
+                stream.Close();
+            }
+            else { MessageBox.Show("Arquivo não encontrado!"); }
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            nome = tbxNome.Text.ToLower();
+
+            SalvarTxt(nome);
+
+
+            MessageBox.Show("Salvo com Sucesso!");
+            tbxNome.Text = "";
+        }
+
+        private void SalvarTxt(string Nome)
+        {
+            Stream stream = File.Open("nome.txt", FileMode.OpenOrCreate);
+            string[] lines = { "First line", "Second line", "Third line" };
+            using (StreamWriter outputFile = new StreamWriter(stream))
+            {
+                foreach (string line in lines)
+                    outputFile.WriteLine(line);
+            }
+            
+           /* StreamWriter escritor = new StreamWriter(stream);
+            escritor.WriteLine(nome);
+            escritor.Close();*/
+            stream.Close();
+        }
+    }
+}
